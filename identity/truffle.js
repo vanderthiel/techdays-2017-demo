5eae51d385d59581f93468e26a82f4ae55e1dd47
Edwin van der Thiel <edwin.vanderthiel@outlook.com>
2017-10-05 17:02:52 +0200

    Added initial contract generated by truffle, configured for techdays migration

---
+++ identity/truffle.js
@@ -0,0 +1,14 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    techdays: {
+      host: "tdola4vww.westeurope.cloudapp.azure.com",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  }
+};
