7db14b7579940e884f78b125105319795745b1dc
Edwin van der Thiel <edwin.vanderthiel@outlook.com>
2017-10-08 22:05:19 +0200

    Delegated Identity contract, demo ready

--- identity/contracts/DelegatedIdentity.sol
+++ identity/contracts/DelegatedIdentity.sol
@@ -1,5 +1,33 @@
 pragma solidity ^0.4.4;
 
 contract DelegatedIdentity {
-    
+    // remember owner
+    address owner;
+
+    // Any public keys
+    mapping (address => bytes32) publicKeys;
+
+    // And all available claims, traceable
+    mapping (string => address) claims;
+
+    function DelegatedIdentity() {
+        owner = msg.sender;
+    }
+
+    function addClaim(string claim) {
+        claims[claim] = msg.sender;
+    }
+
+    function checkClaim(string publicKey, string claim) returns (address authority) {
+        if (sha256(publicKey) == publicKeys[msg.sender]) {
+            // todo: find claim
+            return claims[claim];
+        }
+    }
+
+    function addRequester(address requester, string publicKey) {
+        if (msg.sender == owner) {
+            publicKeys[requester] = sha256(publicKey);
+        }
+    }
 }
